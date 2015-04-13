f8266d60a27d6f64cacc7823485888899cc3d6da
Hamcha <zikyky@gmail.com>
2015-04-13 20:26:58 +0200

    Fix vertex shader

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -2,7 +2,7 @@
 
 const GLchar* vshader[] =
 {
-	"varying vec2 LVertexPos2D;"
+	"attribute vec2 LVertexPos2D;"
 	"void main() {"
 	"	gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 );"
 	"}"
