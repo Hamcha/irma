c2885c6ab0457b526571ee0ae76d5b7684c810e1
Hamcha <zikyky@gmail.com>
2015-04-15 01:25:23 +0200

    [example] Mouse sensitivity

--- projects/example/main.glsl
+++ projects/example/main.glsl
@@ -27,20 +27,22 @@ vec3 hsb(float h, float s, float b) {
 void main() {
     vec2 p = gl_FragCoord.xy/uResolution;
     vec2 ratio = uResolution.xy/uResolution.yy;
+    float sensibility = 1.2;
     vec2 mouse = uMouseCoord/uResolution;
+    mouse = vec2(0.5 - mouse.x, -0.5 + mouse.y) * sensibility;
 
-    vec3 ro = vec3(0,0,uTime*10);
+    vec3 ro = vec3(sin(uTime*2)*2,cos(uTime*2)*2,uTime*10);
     vec3 rd = normalize(vec3((-1.+2.*p)*ratio,2.5));
 
-    rd.yz = rotate(rd.yz, -0.5+mouse.y);
-    rd.xz = rotate(rd.xz, 0.5-mouse.x);
+    rd.yz = rotate(rd.yz, mouse.y);
+    rd.xz = rotate(rd.xz, mouse.x);
 
     vec3 color = vec3(0);
     vec2 t = intersect(ro, rd);
     if (t.y > 0.) {
-		vec3 pos = ro + rd*t.x;
-		vec3 amb = hsb(mod(pos.z/10, 1.0), 0.5, 0.2) + vec3(0, .07, .15);
-		vec3 col = hsb(mod(pos.z/10, 1.0), 1, 0.5);
+        vec3 pos = ro + rd*t.x;
+        vec3 amb = hsb(mod(pos.z/10, 1.0), 0.5, 0.2) + vec3(0, .07, .15);
+        vec3 col = hsb(mod(pos.z/10, 1.0), 1, 0.5);
         vec3 lum = vec3(0.5, 0.5, -2);
         color = amb + col * softshadow(pos, lum, 5) * 50;
     }
